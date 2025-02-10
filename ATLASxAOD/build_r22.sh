c448f45f4e4d67d5e4abbaeb350ab039d2681e53
ponyisi <ponyisi@utexas.edu>
2025-02-10 16:53:36 -0600

    Try a R25 build for ATLASxAOD

--- ATLASxAOD/build_r22.sh
+++ ATLASxAOD/build_r22.sh
@@ -1,1 +1,1 @@
-docker build -t sslhep/servicex_func_adl_xaod_transformer:22.2.107 --build-arg ANALYSISBASE_TAG=22.2.107 .
+docker build -t sslhep/servicex_func_adl_xaod_transformer:25.2.38 --build-arg ANALYSISBASE_TAG=25.2.38 .
