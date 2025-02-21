0bf2f5c98f36aba6e603b6417847c95966491a5b
ponyisi <ponyisi@utexas.edu>
2025-02-21 16:50:37 -0600

    Update ATLAS xAOD images for latest R21 and 22 releases (#27)
    * Update atlas.yaml for latest R21 and 22 releases
    * Update build_r21.sh
    * Update build_r22.sh
    * Update README.md

--- ATLASxAOD/build_r21.sh
+++ ATLASxAOD/build_r21.sh
@@ -1,2 +1,2 @@
-docker build -t sslhep/servicex_func_adl_xaod_transformer:21.2.231 --build-arg ANALYSISBASE_TAG=21.2.231 \
+docker build -t sslhep/servicex_func_adl_xaod_transformer:21.2.283 --build-arg ANALYSISBASE_TAG=21.2.283 \
     --build-arg EL=7 .
